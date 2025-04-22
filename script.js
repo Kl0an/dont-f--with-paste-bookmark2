a4a23e4cf02039ba22a122ea20b29a8a8d25cab3
Daniel Nunes Lima <heoenkasatru2838@gmail.com>
2025-04-22 20:02:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function removeBlock() {
+    const preventDefault = function(e) {
+        e.stopImmediatePropagation();
+        e.preventDefault();
+        return true;
+    };
 
+    document.addEventListener("copy", preventDefault, true);
+    document.addEventListener("cut", preventDefault, true);
+    document.addEventListener("paste", preventDefault, true);
+    
+    alert("Block Removed!");
+}
+
+// Chame a função para remover o bloqueio
+removeBlock();
